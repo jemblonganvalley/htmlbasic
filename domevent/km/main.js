7ed28d718d6e174499c9692b2da5c0d3cb08fd20
jemblonganvalley <jemblonganvalley@gmail.com>
2020-09-23 08:39:18 +0700

    update 230920

---
+++ domevent/km/main.js
@@ -0,0 +1,25 @@
+
+
+//kita buat sebuah fungsi untuk
+//memunculkan MODAL
+
+const showModal = ()=>{
+    //kita dapatkan element MODAL
+    //yang mau di munculkan
+    let modal = document.getElementById('modalMenu')
+
+    //kita atur styling element modalnya
+    modal.style.display = 'flex'
+
+}
+`1  q`
+const closeModal = ()=>{
+  //kita dapatkan element MODAL
+    //yang mau di munculkan
+    let modal = document.getElementById('modalMenu')
+
+    //kita atur styling element modalnya
+    modal.style.display = 'none'
+
+}
+
---
+++ domevent/kp/main.js
@@ -0,0 +1,24 @@
+
+//kita buat sebuah fungsi yang akan 
+//memunculkan MODAL
+
+const showModal = ()=>{
+
+    //kita ambil element yang akan di SHOW
+    let modal = document.querySelector('.modalMenu')
+
+    //kita atus STYLE dari element tersebut
+    //jadikan dia terlihat
+    modal.style.display = 'flex'
+
+}
+
+//kita buat fungsi untuk hide element
+const hideModal = ()=>{
+    //kita ambil element yang akan di SHOW
+    let modal = document.querySelector('.modalMenu')
+
+    //kita atus STYLE dari element tersebut
+    //jadikan dia tersembunyi
+    modal.style.display = 'none'
+}
